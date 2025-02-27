7420326e013c68aa86566e34ed101776dadcb223
yago-gouveia <150040567+yago-gouveia@users.noreply.github.com>
2025-02-27 17:51:03 -0300

    Ajustem e componente Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,24 +1,37 @@
-import Image from "next/image";
-
-
+import Image from 'next/image'
+import ImageLogo from '../app/assets/logo.svg'
 export default function Header() {
   return (
     <>
       <header className="flex top-0 z-11 w-full h-[var(--nav-height)] items-center bg-[#0A192F] bg-opacity-85 backdrop-blur-lg transition-all px-[50px] pointer-events-auto user-select-auto">
-        <nav className="flex items-center justify-between w-full text-[var(--lightest-slate)] font-mono z-12">
+        <nav className="flex items-center justify-between w-full text-[var(--lightest-slate)] z-12">
           <div className="flex items-center justify-center ">
-            <button>
-                
-            </button>
+            <Image src={ImageLogo} alt="" width={150} height={40} />
           </div>
 
-          <div className="flex items-center gap-6 font-mono text-[var(--lightest-slate)]">
+          <div className="flex items-center gap-6 font-fira text-[var(--lightest-slate)]">
             <ol className="flex gap-4 list-decimal list-inside">
-              <li className="mx-1">About</li>
-              <li className="mx-1">Experience</li>
-              <li className="mx-1">Contact</li>
+              <li className="mx-1">
+                <a href="" className="hover:text-[var(--green)]">
+                  About
+                </a>
+              </li>
+              <li className="mx-1">
+                <a href="" className="hover:text-[var(--green)]">
+                  Experience
+                </a>
+              </li>
+              <li className="mx-1">
+                <a href="" className="hover:text-[var(--green)]">
+                  Contact
+                </a>
+              </li>
             </ol>
-            <button className="ml-4 text-[var(--fz-xs)]">Resume</button>
+            <div className="border border-[var(--green)] rounded-md flex items-center justify-center py-3 px-4 hover:bg-[var(--green)] hover:text-[var(--gray-700)] transition duration-300">
+              <a href="" className=" text-[var(--fz-xs)]">
+                Resume
+              </a>
+            </div>
           </div>
         </nav>
       </header>
