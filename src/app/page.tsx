884b201fec40f6d88e748da3beec9ba5c90358c2
yago-gouveia <150040567+yago-gouveia@users.noreply.github.com>
2025-02-28 16:56:36 -0300

    Update: ajuste

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,14 @@
 import Header from '@/src/components/Header'
-import { Instagram, Github, Linkedin } from 'lucide-react';
+import { Instagram, Github, Linkedin } from 'lucide-react'
 export default function Home() {
-    return (
-        <>
-            <Header />
-            <div className="flex">
-                {/* Sidebar (Lateral Esquerda) */}
-                <div className="w-10 fixed bottom-0 left-10 z-10 text-[var(--light-slate)]">
-                    <ul className='flex flex-col items-center'>
-                        <li>
-                            <Instagram />
-                        </li>
-                        <li>
-                            <Github />
-                        </li>
-                        <li>
-                            <Linkedin />
-                        </li>
-                    </ul>
-                </div>
+  return (
+    <>
+      <Header />
 
-                {/* Conteúdo principal (Centralizado) */}
-                <main className="flex-1 flex justify-center items-center min-h-screen bg-gray-100">
-                    Conteúdo principal
-                </main>
-            </div>
-
-
-        </>
-    )
+      {/* Conteúdo principal (Centralizado) */}
+      <main className="flex-1 flex justify-center items-center min-h-screen bg-gray-100">
+        Conteúdo principal
+      </main>
+    </>
+  )
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,39 +1,64 @@
+'use client'
 import Image from 'next/image'
 import ImageLogo from '../app/assets/logo.svg'
+import { AlignJustifyIcon } from 'lucide-react'
+import { useState } from 'react'
 export default function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+
   return (
     <>
-      <header className="flex top-0 z-11 w-full h-[var(--nav-height)] items-center bg-[#0A192F] bg-opacity-85 backdrop-blur-lg transition-all px-[50px] pointer-events-auto user-select-auto">
-        <nav className="flex items-center justify-between w-full text-[var(--lightest-slate)] z-12">
-          <div className="flex items-center justify-center ">
-            <Image src={ImageLogo} alt="" width={140} height={40} />
-          </div>
-
-          <div className="flex items-center gap-6 font-fira text-[var(--lightest-slate)]">
-            <ol className="flex gap-4 list-decimal list-inside marker:text-[var(--green)]">
-              <li className="mx-1">
-                <a href="https://example.com" className="hover:text-[var(--green)]">
-                  About
-                </a>
-              </li>
-              <li className="mx-1">
-                <a href="https://example.com" className="hover:text-[var(--green)]">
-                  Experience
-                </a>
-              </li>
-              <li className="mx-1">
-                <a href="https://example.com" className="hover:text-[var(--green)]">
-                  Contact
-                </a>
-              </li>
-            </ol>
-            <div className="border border-[var(--green)] rounded-md flex items-center justify-center py-3 px-4 hover:bg-[var(--green)] hover:text-[var(--gray-700)] transition duration-300">
-              <a href="https://example.com" className=" text-[var(--fz-xs)]">
-                Resume
+      <header className="flex justify-between items-center text-black py-6 px-8 lg:px-32 bg-amber-400 drop-shadow-md">
+        <div className="">
+          <Image src={ImageLogo} alt="" width={140} height={40} />
+        </div>
+        <nav className="hidden md:flex items-center justify-between gap-8">
+          <ol className="flex gap-6 text-base list-decimal list-inside marker:text-[var(--green)]">
+            <li className="mx-1">
+              <a
+                href="https://example.com"
+                className="p-3 rounded-md transition-all hover:text-[var(--green)]"
+              >
+                About
               </a>
-            </div>
+            </li>
+            <li className="mx-1">
+              <a
+                href="https://example.com"
+                className="p-3 rounded-md transition-all hover:text-[var(--green)]"
+              >
+                Experience
+              </a>
+            </li>
+            <li className="mx-1">
+              <a
+                href="https://example.com"
+                className="p-3 rounded-md transition-all hover:text-[var(--green)]"
+              >
+                Contact
+              </a>
+            </li>
+          </ol>
+
+          <div className="border border-[var(--green)] rounded-md flex items-center justify-center py-3 px-4 hover:bg-[var(--green)] hover:text-[var(--gray-700)] transition duration-300">
+            <a href="https://example.com" className=" text-[var(--fz-xs)]">
+              Resume
+            </a>
           </div>
         </nav>
+        <AlignJustifyIcon
+          className={`md:hidden block text-5xl cursor-pointer ${isMenuOpen ? 'hidden' : ''}`}
+          onClick={() => setIsMenuOpen(isMenuOpen)}
+        />
+
+        <aside
+          className={`fixed md:hidden top-0 right-0 w-60 bottom-0 h-dvh bg-white flex flex-col items-center gap-6 font-semibold text-lg transform transition-transform ${
+            isMenuOpen ? 'opacity-100' : 'opacity-0'
+          }`}
+          style={{ transition: 'transform 0.3s ease, opacity 0.3s ease' }}
+        >
+          <li className='list-none w-full text-center p-4 hover:text-[var(--green)] transition-all cursor-pointer'> Ola</li>
+        </aside>
       </header>
     </>
   )
