52ddcf6e4cdcdb51fdf485da3d0d78ff7231c6db
Yago Barbosa <91913602+YagoB16@users.noreply.github.com>
2025-03-03 00:32:51 -0300

    Remoção de padding na vertical

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -8,10 +8,10 @@ export function Layout({ children }: { children: React.ReactNode }) {
     return (
         <>
             <body >
-                <div className="bg-red-400 flex flex-col min-h-[100vh]">
+                <div className="bg-[var(--navy)] flex flex-col min-h-[100vh]">
                     <Header />
 
-                    <main className="my-0 mx-auto max-w-[1600px] min-h-[100vh] py-50 px-37.5">
+                    <main className="my-0 mx-auto max-w-[1600px] min-h-[100vh] py-0 px-37.5">
                         {children}
                     </main>
                     <footer>
