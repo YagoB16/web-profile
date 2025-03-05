b1bec3e0190c1f422241fcd09efcc0e29c589356
Yago Barbosa <91913602+YagoB16@users.noreply.github.com>
2025-03-05 13:06:44 -0300

    Ajuste em navbar, Position de header é selecionado com conndição

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -20,8 +20,8 @@ export default function NavBar() {
         })
     })
     return (
-        <div className="fixed left-0 right-0 max-w-full flex items-center h-[var(--nav-height)] bg-[var(--navy)] text-[var(--lightest-slate)] md:px-10 px-5 drop-shadow-md">
-            <nav className={`flex w-full justify-between items-center gap-8 z-50 ${isScroll ? " bg-[var(--navy)] opacity-50 shadow-sm" : ""}`}>
+        <div className={`absolute left-0 right-0 max-w-full flex items-center h-[var(--nav-height)] bg-[var(--navy)] min-h-12 ${isScroll ? "fixed bg-[var(--navy)] opacity-50 shadow-sm drop-shadow-md" : ""} ${isMenuOpen ? "opacity-100" : ""} text-[var(--lightest-slate)] md:px-10 px-5 `}>
+            <nav className="flex w-full justify-between items-center gap-8 z-50 ">
                 <div>
                     <Image src={ImageLogo} alt="Logo" width={140} height={40} />
                 </div>
@@ -48,13 +48,16 @@ export default function NavBar() {
                                 Contact
                             </a>
                         </li>
+                        <li>
+                            <a
+                                href="https://example.com"
+                                className="border border-[var(--green)] rounded-md py-2 px-4 transition duration-300 hover:bg-[var(--green)] hover:text-[var(--gray-700)] ml-2.5"
+                            >
+                                Resume
+                            </a>
+                        </li>
                     </ul>
-                    <a
-                        href="https://example.com"
-                        className="border border-[var(--green)] rounded-md py-3 px-4 transition duration-300 hover:bg-[var(--green)] hover:text-[var(--gray-700)] ml-2.5"
-                    >
-                        Resume
-                    </a>
+
                 </div>
 
                 <div className='md:hidden'>
